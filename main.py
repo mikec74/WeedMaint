ff33268829934c98eadfc1567eae895458f30f43
Mike <mikepy74@gmail.com>
2022-03-08 19:43:47 -0600

    Add announcement column to PageHeader. Load data for two weeds.

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ class PageHeader(db.Model):
     image_url = db.Column(db.String(250), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     subtitle = db.Column(db.String(100), nullable=False)
+    announcement = db.Column(db.Text, nullable=True)
 
 
 class Weed(db.Model):
@@ -71,6 +72,7 @@ class WeedPhoto(db.Model):
     weed = db.relationship("Weed", backref=db.backref("weed_photos"), lazy=True)
 
 
+db.drop_all()
 db.create_all()
 
 
@@ -80,11 +82,14 @@ def show_page():
         image_url="https://images.unsplash.com/photo-1631163468569-b5265125d578?ixlib=rb-1.2.1"\
                   "&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
         title="Weed of the Week",
-        subtitle="These invasives have to go!"
+        subtitle="These invasives have to go!",
+        announcement="<strong>IMPORTANT NOTE:</strong> The Palatine Park District is scheduled to mow the "\
+                     "ditch this week. The Park District uses heavy equipment to mow the ditch. For your safety, "\
+                     "do not go into the ditch if the Park District is mowing the ditch."
     )
     db.session.add(page_header)
 
-    weed = Weed(
+    weed1 = Weed(
         scientific_name="Barbarea vulgaris",
         description="This plant is easily identified by its clusters of yellow flowers. It is the only plant "\
                     "that is blooming in the ditch with yellow flowers this week. The plant is around "\
@@ -104,59 +109,132 @@ def show_page():
         common_name="yellow rocket",
         is_primary=True
     )
-    weed.weed_common_names.append(yellow_rocket)
+    weed1.weed_common_names.append(yellow_rocket)
 
     wintercress = WeedCommonName(
         common_name="wintercress",
         is_primary=False
     )
-    weed.weed_common_names.append(wintercress)
+    weed1.weed_common_names.append(wintercress)
 
     herb_barbara = WeedCommonName(
         common_name="herb barbara",
         is_primary=False
     )
-    weed.weed_common_names.append(herb_barbara)
+    weed1.weed_common_names.append(herb_barbara)
 
     rocketcress = WeedCommonName(
         common_name="rocketcress",
         is_primary=False
     )
-    weed.weed_common_names.append(rocketcress)
+    weed1.weed_common_names.append(rocketcress)
 
     yellow_rocketcress = WeedCommonName(
         common_name="yellow rocketcress",
         is_primary=False
     )
-    weed.weed_common_names.append(yellow_rocketcress)
+    weed1.weed_common_names.append(yellow_rocketcress)
 
     winter_rocket = WeedCommonName(
         common_name="winter rocket",
         is_primary=False
     )
-    weed.weed_common_names.append(winter_rocket)
+    weed1.weed_common_names.append(winter_rocket)
 
     wound_rocket = WeedCommonName(
         common_name="wound rocket",
         is_primary=False
     )
-    weed.weed_common_names.append(wound_rocket)
+    weed1.weed_common_names.append(wound_rocket)
 
     photo1 = WeedPhoto(
         photo_url="images/YellowRocketInDitch.jpg",
         caption="Lots of yellow rocket",
         display_order=2
     )
-    weed.weed_photos.append(photo1)
+    weed1.weed_photos.append(photo1)
 
     photo2 = WeedPhoto(
         photo_url="images/YellowRocketNOT.jpg",
         caption="This is <strong>NOT</strong> <i>Barbarea vulgaris</i>",
         display_order=1
     )
-    weed.weed_photos.append(photo2)
+    weed1.weed_photos.append(photo2)
 
-    db.session.add(weed)
+    db.session.add(weed1)
+    db.session.commit()
+
+    weed2 = Weed(
+        scientific_name="Leucanthemum vulgare",
+        description="This plant has the typical daisy appearance with white petals and a yellow center. It grows "
+                    "to a height of two feet.",
+        removal_method="Pull the plant out by the roots. This "
+                       "plant has a strong and deep root system, so you usually will not be able to pull out "
+                       "all of the roots. You may have more luck pulling out more roots when the soil is moist "
+                       "after a rain. When you pull the plant, make sure you put "
+                       "the entire plant in a yard waste bag and remove the bag from the premises.",
+        comments="This is a perennial from Eurasia.",
+        location_desc="Along the western edge of the ditch.",
+        location_map="images/YellowRocket-20220307.png",
+        display_order=1
+    )
+    oxeye_daisy = WeedCommonName(
+        common_name="oxeye daisy",
+        is_primary=True
+    )
+    weed2.weed_common_names.append(oxeye_daisy)
+
+    ox_eye_daisy = WeedCommonName(
+        common_name="ox-eye daisy",
+        is_primary=False
+    )
+    weed2.weed_common_names.append(ox_eye_daisy)
+
+    dog_daisy = WeedCommonName(
+        common_name="dog daisy",
+        is_primary=False
+    )
+    weed2.weed_common_names.append(dog_daisy)
+
+    marguerite = WeedCommonName(
+        common_name="marguerite",
+        is_primary=False
+    )
+    weed2.weed_common_names.append(marguerite)
+
+    bull_daisy = WeedCommonName(
+        common_name="yellow bull daisy",
+        is_primary=False
+    )
+    weed2.weed_common_names.append(bull_daisy)
+
+    button_daisy = WeedCommonName(
+        common_name="button daisy",
+        is_primary=False
+    )
+    weed2.weed_common_names.append(button_daisy)
+
+    field_daisy = WeedCommonName(
+        common_name="field daisy",
+        is_primary=False
+    )
+    weed2.weed_common_names.append(field_daisy)
+
+    photo3 = WeedPhoto(
+        photo_url="images/OxeyeDaisy-2.jpg",
+        caption="Oxeye daisy stem has very small leaves",
+        display_order=2
+    )
+    weed2.weed_photos.append(photo3)
+
+    photo4 = WeedPhoto(
+        photo_url="images/OxeyeDaisy-1.jpg",
+        caption="The classic daisy look",
+        display_order=1
+    )
+    weed2.weed_photos.append(photo4)
+
+    db.session.add(weed2)
     db.session.commit()
 
     return render_template("index.html")
